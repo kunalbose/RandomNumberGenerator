938867b9b0b4a1ecd2d678de2d0824da8e4d3787
kunalbose <kunalbose234@gmail.com>
2021-01-12 13:58:33 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var n=prompt("Enter the upper bound");
+function gen(n){
+    let randomNum=Math.round(Math.random()*n);
+    console.log(randomNum);
+    document.querySelector("h2").innerText="Refresh to Generate Random Number Between 0 to "+n;
+    document.querySelector("p").innerText=randomNum;
+}
+
+gen(n);
